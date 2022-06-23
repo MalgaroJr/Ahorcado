0dd4662096a9b8383e29193a8fb8ca37a5eb2604
Tomás Malgarín <malgarojr.tm@gmail.com>
2022-06-22 21:31:42 -0300

    Added Sing in
    Se murió un oso panda

--- TestUI/UnitTest1.cs
+++ TestUI/UnitTest1.cs
@@ -1,16 +1,33 @@
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace TestUI
 {
     public class Tests
     {
+        public IWebDriver driver;
+        public string site;
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            site = "https://google.com";
         }
+
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl(site);
+            By searchBar = By.Name("q");
+            By btnSearch = By.Name("btnK");
+            
+            driver.FindElement(searchBar).SendKeys("skere");
+           
+            driver.FindElement(btnSearch).Click();
+            Assert.IsTrue(true);
         }
     }
 }
